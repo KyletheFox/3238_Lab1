c71e9f7acc3056e025bd320fe09a32247e6a7a82
kylethefox <halobuster1982>
2016-09-12 15:16:17 -0400

    Init Commit

---
+++ src/Date.java
@@ -0,0 +1,49 @@
+
+public class Date {
+	
+	private int year;
+	private int month;
+	private int day;
+	
+	public Date(int year, int month, int day) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+	
+	public int daysTo(Date dateTo) {
+		
+		int days = 0;
+		int yDif, mDif, dDif;
+		
+		// Get the difference between years month days
+		yDif = dateTo.year - this.year;
+		
+		mDif = dateTo.month - this.month;
+		
+		if (mDif < 0) {
+			mDif += 12;
+			yDif--;
+		}
+		
+		dDif = (dateTo.day - this.day) % 30;
+		
+		if (dDif < 0) {
+			dDif += 30;
+			mDif--;
+		}
+		
+//		System.out.println(yDif);
+//		System.out.println(mDif);
+//		System.out.println(dDif);
+		
+		// Add difference of days
+		days += yDif * 365;
+		days += mDif * 30;
+		days += dDif;
+		
+		return days;
+	}
+	
+
+}
---
+++ src/dateTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class dateTest {
+	
+	Date d1 = new Date(2002, 9, 11);
+	Date d2 = new Date(2003, 3, 2);
+
+	@Test
+	public void test1() {
+		assertEquals(171, d1.daysTo(d2));
+	}
+	
+	Date d3 = new Date(2008, 1, 1);
+	Date d4 = new Date(2007, 1, 1);
+
+	@Test
+	public void test2() {
+		assertEquals(-365, d3.daysTo(d4));
+	}
+	
+	Date d5 = new Date(2000, 1, 1);
+	Date d6 = new Date(2001, 2, 1);
+	
+	@Test
+	public void test3() {
+		assertEquals(395, d5.daysTo(d6));
+	}
+	
+}
